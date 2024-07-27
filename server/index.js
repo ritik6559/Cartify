083b593ec9442025f2bee6ed0d2a4e2ddc8e7202
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-28 01:14:13 +0530

    writing first api in express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 console.log("Hello, world");
+
+const express = require('express');//importing express
+
+const PORT = 3000;//this is a template
+
+const app = express();
+//CREATING AN API
+
+app.listen(PORT,"0.0.0.0" ,() => {
+    console.log(`connected at port ${PORT}`);
+});
+
