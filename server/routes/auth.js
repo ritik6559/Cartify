22f0f87b9a18334519da29e012e01a7ce5f25670
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-28 23:29:46 +0530

    incrypting password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 const authRouter = express.Router();
 
@@ -19,9 +20,11 @@ authRouter.post('/api/signup',async (req,res) => {
         return res.status(400).json({msg: 'User with same email already exists'});
     } 
 
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
     let user = new User({
         email,
-        password,
+        password : hashedPassword,
         name,
     })
 
