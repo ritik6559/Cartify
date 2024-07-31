351266c337a9181b8d2b966ace1786798b72b27e
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-31 19:03:05 +0530

    account screen ui completed

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -1,4 +1,7 @@
 import 'package:e_commerce_application/constants/global_variables.dart';
+import 'package:e_commerce_application/features/account/widgets/below_app_bar.dart';
+import 'package:e_commerce_application/features/account/widgets/orders.dart';
+import 'package:e_commerce_application/features/account/widgets/top_buttons.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -11,16 +14,48 @@ class AccountScreen extends StatelessWidget {
         preferredSize: const Size.fromHeight(50),
         child: AppBar(
           title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
           flexibleSpace: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               gradient: GlobalVariables.appBarGradient,
             ),
           ),
         ),
+      ),
+      body: const Column(
+        children: [
+          BelowAppBar(),
+          SizedBox(height: 10),
+          TopButtons(),
+          SizedBox(height: 20),
+          Orders(),
+        ],
       ),
     );
   }
---
+++ lib/features/account/widgets/orders.dart
@@ -0,0 +1,63 @@
+import 'package:e_commerce_application/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({super.key});
+
+  @override
+  State<Orders> createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                left: 15,
+              ),
+              child: const Text(
+                'Your orders',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                right: 15,
+              ),
+              child: Text(
+                'See All',
+                style: TextStyle(
+                  color: GlobalVariables.selectedNavBarColor,
+                ),
+              ),
+            ),
+            //display orders
+          ],
+        ),
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Image.asset(
+                'assets/images/mobiles.jpeg',
+                height: 170,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
