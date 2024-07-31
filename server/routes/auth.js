670113fa37fbb0222dd52287115372e00526a33c
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-31 18:04:40 +0530

    fixing errors

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,31 +11,31 @@ authRouter.post("/api/signup",async (req,res) => {
     try{
         const {name, email, password} = req.body;
         const existingUser = await User.findOne({ email });//we are trying to find the user same email in the user collection.
-    if(existingUser){
-        return res.status(400).json({msg: 'User with same email already exists'});
-    } 
+        if(existingUser){
+            return res.status(400).json({msg: 'User with same email already exists'});
+        } 
 
-    const hashedPassword = await bcryptjs.hash(password, 8);
+        const hashedPassword = await bcryptjs.hash(password, 8);
 
-    let user = new User({
-        email,
-        password : hashedPassword,
-        name,
-    })
+        let user = new User({
+            email,
+            password : hashedPassword,
+            name,
+        });
 
-    user = await user.save();
-    res.json(user);
+        user = await user.save();
+        res.json(user);
     } catch (e) {
         res.status(500).json({error: e.message });
     }
-})
+});
 
 //signin route
 authRouter.post("/api/signin",async (req,res) => {
     try{
         const {email,password} = req.body;
 
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });
         if(!user){
             return res
                 .status(400)
@@ -60,34 +60,29 @@ authRouter.post("/api/signin",async (req,res) => {
 
 });
 
-authRouter.post("/tokenIsValid",async (req,res) => {
-    try{
-        const token = req.header('x-auth-token');
-        //checking if token is present or not.
-        if(!token){
-            return res.json(false);
-        }
+authRouter.post("/tokenIsValid", async (req, res) => {
+    try {
+      const token = req.header("x-auth-token");
+      //checking if token is present or not.
+      if (!token) return res.json(false);
+      //if token is present checking if the token is correct or verifying the token.
+      const verified = jwt.verify(token, "passwordKey");
+      if (!verified) return res.json(false);
 
-        //checking if token found is correct.
-        const isVerified = jwt.verify(token, 'passwordKey');
-        if(!isVerified){
-            return res.json(false);
-        }
-        //checking if the token found is not a random token and is legit.
-        const user = await User.findById(jwt.verify.id);
-        if(!user){
-            res.json(false);
-        }
-        res.json(true);
-    } catch(e){
-        res.status(500).json({error: e.message});
+
+      //checking if the token generated is not a random token.
+      const user = await User.findById(verified.id);
+      if (!user) return res.json(false);
+      res.json(true);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
-});
+  });
 
-authRouter.get('/', auth,async(req,res) => {
+  authRouter.get("/", auth, async (req, res) => {
     const user = await User.findById(req.user);
-    res.json({...user._doc,token: req.token});
-});
+    res.json({ ...user._doc, token: req.token });
+  });
 
 
 
