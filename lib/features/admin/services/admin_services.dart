554b788ab1e705e989abb74a7ce41963dc0a8ea4
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-02 23:14:42 +0530

    adding product model

---
+++ lib/features/admin/services/admin_services.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:e_commerce_application/constants/utils.dart';
+import 'package:flutter/material.dart';
+
+class AdminServices {
+  void sellProducts({
+    required BuildContext context,
+    required String name,
+    required String description,
+    required double price,
+    required double quantity,
+    required String category,
+    required List<File> images,
+  }) async {
+    try {
+      final cloudinary = CloudinaryPublic('ddfhvzddv', 'rxfjvmid');
+      List<String> imageUrls = [];
+      for (int i = 0; i < images.length; i++) {
+        //getting images path and uploading to cloudinary.
+        CloudinaryResponse res = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(images[i].path,folder: name),//creating folder for each product and stroing product images in that folder.
+        );
+        imageUrls.add(res.secureUrl);//it's the same as download url that we get in firebase.
+      }
+
+      //we will be storing images in cloudinary and urls in mongodb as we are getting limited memory(512 Mb) that is not sufficient.
+
+
+    } catch (e) {
+      showSnackBar(
+        context,
+        e.toString(),
+      );
+    }
+  }
+}
